18705c9e073fd6286a62285bd9d5c705ddd70c66
Paulo Guillen <pcguillen@gmail.com>
2019-10-10 19:27:18 -0500

    Font plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +57,8 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableToolbar,
 	Underline,
-	Strikethrough
+	Strikethrough,
+	Font
 ];
 
 // Editor configuration.
